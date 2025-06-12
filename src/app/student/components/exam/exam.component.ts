d419e0a2cbb03084906ee178db3951a7348b3af0
Rayane300402 <raycaratboulsi@gmail.com>
2025-06-12 15:29:18 +0300

    refactor exam component: update user role handling, improve answer selection logic, and enhance result calculation

--- src/app/student/components/exam/exam.component.ts
+++ src/app/student/components/exam/exam.component.ts
@@ -11,13 +11,14 @@ import { AuthService } from '../../../auth/services/auth.service';
 })
 export class ExamComponent implements OnInit {
 
-  id:any;
-  subject:any;
-  user:any = {};
-
-  constructor(private route: ActivatedRoute, private doctorService: DoctorService, private toastr: ToastrService, private auth: AuthService) { 
+  id: any;
+  subject: any;
+  user: any = {};
+  total:number = 0;
+  showResult: boolean = false;
+  constructor(private route: ActivatedRoute, private doctorService: DoctorService, private toastr: ToastrService, private auth: AuthService) {
     this.id = this.route.snapshot.paramMap.get('id');
-    console.log('Exam ID:', this.id);
+    // console.log('Exam ID:', this.id);
   }
 
   ngOnInit(): void {
@@ -27,26 +28,26 @@ export class ExamComponent implements OnInit {
 
   getSubject() {
     this.doctorService.getSubject(this.id).subscribe((res: any) => {
-      console.log('Exam Data:', res);    
+      // console.log('Exam Data:', res);    
       this.subject = res;
     });
   }
 
-    getUserInfo(){
-    this.auth.getRole().subscribe((res:any)=>{
+  getUserInfo() {
+    this.auth.getRole().subscribe((res: any) => {
       this.user = res;
     }
-    , (err:any)=>{
-      console.error('Error fetching user info:', err); 
-    }
+      , (err: any) => {
+        console.error('Error fetching user info:', err);
+      }
     );
   }
 
-    delete(index: number) {
+  delete(index: number) {
     this.subject.questions.splice(index, 1);
     const model = {
       name: this.subject,
-      questions:  this.subject.questions
+      questions: this.subject.questions
     }
     this.doctorService.updateSubject(model, this.id).subscribe((res: any) => {
       console.log(res);
@@ -56,6 +57,28 @@ export class ExamComponent implements OnInit {
       this.toastr.error('حدث خطأ ما أثناء حذف السؤال');
     });
 
+  }
+
+  getAnswer(event: any) {
+    let value = event.value,
+      questionIndex = event.source.name;
+
+      this.subject.questions[questionIndex].selectedAnswer = value; // Store the selected answer in the question object, creating a new property
+      console.log('Updated Question:', this.subject.questions);
+    console.log('Selected Answer:', event);
+  }
+
+  getResult() {
+    this.total = 0; // Reset total before calculating
+
+    this.subject.questions.forEach((question: any) => {
+      if (question.selectedAnswer == question.correctAnswer) {
+        this.total++;
+      }
+    });
+    console.log('Total Correct Answers:', this.total);
+    this.toastr.success(`تم الانتهاء من الامتحان بنجاح`);
+    this.showResult = true; // Show the result after calculation
   }
 
 }
